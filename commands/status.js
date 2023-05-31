88d7d7921601a8cac321a24e47114b65a312e3fb
shawzyy1337 <shawzyy1337@gmail.com>
2023-05-31 11:45:50 +0100

    Finished the status command

--- commands/status.js
+++ commands/status.js
@@ -1,27 +1,43 @@
 const { MessageEmbed } = require("discord.js");
+const axios = require("axios");
 
 module.exports = {
   data: {
     name: "status",
-    description: "Status!",
+    description: "Exibe o status do servidor de Minecraft Brawl.",
   },
   async execute(interaction) {
     if (interaction.replied) return;
 
-    const serverIP = "redebrawl.com";
-    const lojaURL = "https://loja.redebrawl.com";
-    const serverStatus = "ON";
-    const playersOnline = "10/20";
-    const motd = "Bem-vindo ao servidor RedeBrawl!";
+    try {
+      const response = await axios.get(
+        "https://api.mcsrvstat.us/2/redebrawl.com"
+      );
 
-    const embed = new MessageEmbed()
-      .setTitle("Status do Servidor de Minecraft")
-      .addField("IP", serverIP)
-      .addField("Loja", `[Clique aqui](${lojaURL})`)
-      .addField("Status", serverStatus)
-      .addField("Jogadores Online", playersOnline)
-      .addField("MOTD", motd);
+      const serverIP =
+        "[redebrawl.com](https://pt.namemc.com/server/redebrawl.com)";
+      const lojaURL = "https://loja.redebrawl.com";
+      const serverStatus = response.data.online ? "`ONLINE`" : "`OFFLINE`";
+      const playersOnline =
+        "`" +
+        `${response.data.players.online}/${response.data.players.max}` +
+        "`";
+      const motd = "```" + response.data.motd.clean.join("\n") + "```";
 
-    await interaction.reply({ embeds: [embed] });
+      const embed = new MessageEmbed()
+        .setTitle("Status do Servidor de Minecraft")
+        .addField("IP", serverIP, true)
+        .addField("\u200B", "\u200B", true)
+        .addField("Loja", `[loja.redebrawl.com](${lojaURL})`, true)
+        .addField("Status", serverStatus, true)
+        .addField("\u200B", "\u200B", true)
+        .addField("Jogadores Online", playersOnline, true)
+        .addField("MOTD", motd);
+
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error("Erro ao obter os dados da API:", error);
+      await interaction.reply("Ocorreu um erro ao obter os dados do servidor.");
+    }
   },
 };
